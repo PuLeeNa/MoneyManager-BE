2a3762b3bf02071c20d7ff8d3fa5227ab63230d2
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-11-16 02:56:02 +0530

    Add ProfileController, ProfileService methods, and update ProfileRepository for user registration

--- src/main/java/com/puLeeNa/moneymanager/service/ProfileService.java
+++ src/main/java/com/puLeeNa/moneymanager/service/ProfileService.java
@@ -1,12 +1,46 @@
 package com.puLeeNa.moneymanager.service;
 
+import com.puLeeNa.moneymanager.dto.ProfileDTO;
+import com.puLeeNa.moneymanager.entity.ProfileEntity;
 import com.puLeeNa.moneymanager.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileService {
 
     private final ProfileRepository profileRepository;
+
+    public ProfileDTO registerProfile(ProfileDTO profileDTO) {
+        ProfileEntity newProfile = toEnity(profileDTO);
+        newProfile.setActivationToken(UUID.randomUUID().toString());
+        newProfile = profileRepository.save(newProfile);
+        return toDTO(newProfile);
+    }
+
+    public ProfileEntity toEnity(ProfileDTO profileDTO) {
+        return ProfileEntity.builder()
+                .id(profileDTO.getId())
+                .fullName(profileDTO.getFullName())
+                .email(profileDTO.getEmail())
+                .password(profileDTO.getPassword())
+                .profileImageUrl(profileDTO.getProfileImageUrl())
+                .createdAt(profileDTO.getCreatedAt())
+                .updatedAt(profileDTO.getUpdatedAt())
+                .build();
+    }
+
+    public ProfileDTO toDTO(ProfileEntity profileEntity) {
+        return ProfileDTO.builder()
+                .id(profileEntity.getId())
+                .fullName(profileEntity.getFullName())
+                .email(profileEntity.getEmail())
+                .profileImageUrl(profileEntity.getProfileImageUrl())
+                .createdAt(profileEntity.getCreatedAt())
+                .updatedAt(profileEntity.getUpdatedAt())
+                .build();
+    }
 }
